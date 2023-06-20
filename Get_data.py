a677d24790683fc56abccbe2e091c572711329d9
SaraEersel <455071@student.fontys.nl>
2023-06-20 13:59:51 +0200

    Tested

--- Get_data.py
+++ Get_data.py
@@ -5,34 +5,34 @@ import datetime
 robot_count = 4
 
 def print_message(client, userdata, msg):
-    timestamp = datetime.time()
-    print('Received a new temperature data ', str(msg.payload.decode('utf-8')))
-    print(timestamp)
+    print('Received a new odom data ', str(msg.payload.decode('utf-8')))
     print('message topic=', msg.topic)
     print('message qos=', msg.qos)
 
-def print_message_v(msg):
-    timestamp = datetime.datetime.today()
-    print('Received a new temperature data ', msg)
-    print(timestamp.time())
+    #use a diffrent file for each robot
+    robot_number = 1
+    with open(f'robot{robot_number}.txt', 'a') as f:
+        f.write(str(msg.payload.decode('utf-8') + ', ' + str(datetime.datetime.now().time())))
+        f.write('\n')
 
 # Give a name to this MQTT client
 client = mqtt.Client('test_evaluation')
-for i in range(robot_count):
-    robot_topics = f"r{i}"
-    client.message_callback_add('/{robot_topics}/#', print_message)
-    print_message_v(robot_topics)
+# for i in range(robot_count):
+#     robot_topics = f"r{i}"
+#     client.message_callback_add('/{robot_topics}/#', print_message)
+#     print(f"Added callback for {robot_topics}")
+client.message_callback_add('odom', print_message)
 
-# IP address of your MQTT broker, using ipconfig to look up it  
-client.connect('192.168.1.101', 1883)
-# 'greenhouse/#' means subscribe all topic under greenhouse
+# IP address of your MQTT broker 
+client.connect('192.168.75.201', 1883)
 
-
-
-for i in range(robot_count):
-    robot_topics = f"r{i}"
-    client.subscribe('/{robot_topics}/#')
+# for i in range(robot_count):
+#     robot_topics = f"r{i}"
+#     client.subscribe('/{robot_topics}/#')
+#     print(f"Subscribed to {robot_topics}")
+client.subscribe('odom', qos=1)
 
 client.loop_forever()
+
 # stop the loop
 # client.loop_stop()
